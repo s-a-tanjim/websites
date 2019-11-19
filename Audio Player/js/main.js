118e07e2cca1793a50b5801b01411461ac36b8cb
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2019-11-20 01:39:30 +0600

    algorithm changed and bug fixed

--- Audio Player/js/main.js
+++ Audio Player/js/main.js
@@ -1,14 +1,33 @@
 var audio = document.getElementById('audio');
 
+var allMusic, totalMusicCount = 0,
+  currentlyPlayingMusic = -1;
+
 var pauseFlag = 0,
   playFlag = 0;
 
+// Event Listeners.........................
+
+audio.addEventListener('ended', nextMusic);
+
+// Controls.........................
+
 function nextMusic() {
-  console.log('next');
+  if (parseInt(currentlyPlayingMusic) + 1 == totalMusicCount) {
+    //No song available
+    alert("Add more song please!");
+    return;
+  }
+  changeMusicAt(parseInt(currentlyPlayingMusic) + 1);
 }
 
 function prevMusic() {
-  console.log('prev');
+  if (parseInt(currentlyPlayingMusic) == 0) {
+    //Can't go previous
+    alert("Can't go previous!");
+    return;
+  }
+  changeMusicAt(parseInt(currentlyPlayingMusic) - 1);
 }
 
 function pauseMusic() {
@@ -33,12 +52,14 @@ function playMusic() {
   }
 }
 
+// Controls end!..................
+
 function hoverEffect() {
-  toggleClassUsingID("toggleEffect", "up");
+  //toggleClassUsingID("toggleEffect", "up");
 }
 
 function afterHoverEffect() {
-  toggleClassUsingID("toggleEffect", "up");
+  //toggleClassUsingID("toggleEffect", "up");
 }
 
 function toggleClassUsingClass(Class1, Class2) {
@@ -56,86 +77,128 @@ function isPlaying(audelem) {
 }
 
 
-/* Upload music */
+/* Upload music................... */
 
-let dropArea = document.getElementById('drop-area');
-    ;
-    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-      dropArea.addEventListener(eventName, preventDefaults, false)
-    })
+let dropArea = document.getElementById('drop-area');;
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
 
-    function preventDefaults(e) {
-      e.preventDefault()
-      e.stopPropagation()
-    };
-    ['dragenter', 'dragover'].forEach(eventName => {
-      dropArea.addEventListener(eventName, highlight, false)
-    })
+function preventDefaults(e) {
+  e.preventDefault()
+  e.stopPropagation()
+};
+['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+})
 
-    ;
-    ['dragleave', 'drop'].forEach(eventName => {
-      dropArea.addEventListener(eventName, unhighlight, false)
-    })
+;
+['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
 
-    function highlight(e) {
-      dropArea.classList.add('highlight')
-    }
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
 
-    function unhighlight(e) {
-      dropArea.classList.remove('highlight')
-    }
-    dropArea.addEventListener('drop', handleDrop, false)
+function unhighlight(e) {
+  dropArea.classList.remove('highlight')
+}
+dropArea.addEventListener('drop', handleDrop, false)
 
-    function handleDrop(e) {
-      let dt = e.dataTransfer
-      let files = dt.files
+function handleDrop(e) {
+  let dt = e.dataTransfer;
+  let files = dt.files;
 
-      handleFiles(files)
-    }
+  handleFiles(files);
+}
 
-    function handleFiles(files) {
-      files = [...files]
-      files.forEach(previewFile)
-    }
+function handleFiles(files) {
+  files = [...files];
+  files.forEach(addToList);
+}
 
 
-    function previewFile(file) {
-      var file_name = file.name.substring(0, file.name.length - 4);
-      
-      document.getElementById('song-name-id').innerHTML = file_name;
-     
-      let reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = function () {
-        //let audioFile = document.createElement('audio');
-        //audioFile.src = reader.result;
-        //audioFile.controls=true;
-        //console.log(audioFile);
-        //document.getElementById('playlistWithPlayer').appendChild(audioFile);
-        
-        //List create
+function addToList(file) {
+  var file_name = file.name.substring(0, file.name.length - 4);
 
-        var list=document.createElement('li');
-        var a=document.createElement('a');
-        a.setAttribute('href',reader.result);
-        a.setAttribute('class','musicList-a');
-        a.innerHTML=file_name;
-        a.setAttribute('onclick','changeMusic(this)');
-        list.appendChild(a);
-        list.setAttribute('class','musicList-li');
-        console.log(list);
-        document.getElementById('playlist').appendChild(list);
-      }
-    }
+  var list = document.createElement('li');
+  var p = document.createElement('p');
+  var musicURL = document.createElement('p');
+  p.setAttribute('class', 'musicList-p');
+  p.setAttribute('id', totalMusicCount);
+  totalMusicCount++;
+  p.innerHTML = file_name;
+  p.setAttribute('onclick', 'changeMusic(this.parentElement)');
+  list.appendChild(p);
+  list.setAttribute('class', 'musicList-li');
+  //console.log(list);
+  document.getElementById('playlist').appendChild(list);
+  //store url of music
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function () {
+    musicURL.innerHTML = reader.result;
+    musicURL.setAttribute('class', 'musicURLData');
+    list.appendChild(musicURL);
+  }
+}
 
-    function changeMusic(data){
-      var x=document.getElementById('audio');
-      console.log('change');
-      console.log(data.innerHTML);
-      document.getElementById('song-name-id').innerHTML = data.innerHTML;
-      x.src=data.getAttribute('href');
-      x.load();
-    }
+
+/* Upload music end................... */
+
+// Changing music..........................
+
+function changeMusic(data) {
+
+  var x = document.getElementById('audio');
+  changeSongName(data.childNodes[0].innerHTML);
+  x.src = data.childNodes[1].innerHTML;
+  x.load();
+  x.play();
+  if (currentlyPlayingMusic != -1)
+    selectedMusicChange(parseInt(currentlyPlayingMusic)); // unselect present
+  currentlyPlayingMusic = data.childNodes[0].getAttribute('id');
+  selectedMusicChange(parseInt(currentlyPlayingMusic)); // select present
+
+  console.log("Currently playing: " + currentlyPlayingMusic);
+}
+
+function changeMusicAt(index) {
+  index = parseInt(index);
+  var temp = document.getElementById('playlist').getElementsByTagName("li");
+  var data = temp[index];
+  var x = document.getElementById('audio');
+
+  changeSongName(data.childNodes[0].innerHTML);
+  x.src = data.childNodes[1].innerHTML;
+  x.load();
+  x.play();
+  if (currentlyPlayingMusic != -1)
+    selectedMusicChange(parseInt(currentlyPlayingMusic)); // unselect present
+  currentlyPlayingMusic = data.childNodes[0].getAttribute('id');
+  selectedMusicChange(parseInt(currentlyPlayingMusic)); // select present
+
+  console.log("Currently playing: " + currentlyPlayingMusic);
+}
+
+// Changing music end!!...............
+
+// UI
+
+function selectedMusicChange(index) {
+  index = parseInt(index);
+  var element = document.getElementById(index);
+  element.classList.toggle('musicListSelected-p');
+}
+
+function changeSongName(name){
+  if(name.length>=25){
+    name = name.substring(0, 21);
+    name=name+"...";
+  }
+  document.getElementById('song-name-id').innerHTML = name;
+}
 
 /*
 //like & shuffle button
